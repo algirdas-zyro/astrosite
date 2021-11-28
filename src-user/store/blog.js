931cf0027d0deee360a346bd58f7a52bb1590c6c
ratautas <algirdas@zyro.com>
2021-11-28 15:54:39 +0200

    Remove redundand files

--- src-user/store/blog.js
+++
@@ -1,7 +0,0 @@
-export default {
-	namespaced: true,
-	getters: {
-		categoryNameById: (state, getters, rootState) => (categoryId) => rootState
-			.website.blogCategories[categoryId].name,
-	},
-};
